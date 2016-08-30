44692a17c909ef268dd3cb90e949f2d925ec207e
David Whitlark <whitlark@gmail.com>
2016-08-30 14:50:48 -0400

    tooling collection

--- didact/src/app.jsx
+++ didact/src/app.jsx
@@ -1,3 +1,26 @@
 import './app.less';
 
-console.log('hello world');
+class Test {
+
+    static defaultProps = {
+        autoPlay: false,
+        maxLoops: 10,
+    }
+
+    static ops() {
+        console.log(new Test(), Test.defaultProps);
+    }
+
+    prop = 'awesome'
+
+    constructor() {
+    }
+
+    handleClick = (e) => {
+        this.prop = false;
+    }
+
+}
+
+Test.ops();
+
