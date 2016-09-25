a3853bfe01d3cb8c66986a5b0665f107aa549cb5
David Whitlark <whitlark@gmail.com>
2016-09-25 06:09:12 -0400

    learning seneca

--- exocortex/stem-mesh/src/index.js
+++ exocortex/stem-mesh/src/index.js
@@ -1,2 +1,17 @@
 
-console.log('hi');
+import seneca from 'seneca';
+import senecaMesh from 'seneca-mesh';
+
+const opts = {
+    seneca: {
+        tag: process.env.STEM_MESH_TAG || 'stem-mesh',
+    },
+    mesh: {
+        isbase: true,
+        host: process.env.STEM_MESH_HOST || '127.0.0.1',
+        port: process.env.STEM_MESH_PORT || 39000,
+        bases: (process.env.STEM_MESH_BASES || '').split(','),
+    }
+};
+
+seneca(opts.seneca).use(senecaMesh, opts.mesh);
