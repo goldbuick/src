d2c4e648956cb3aea35a69518c14df96cd6f5e73
David Whitlark <whitlark@gmail.com>
2017-05-12 07:36:08 -0400

    doing some re-org, thinking of future work

--- seraphim/src/apps/test/index.js
+++ seraphim/src/apps/test/index.js
@@ -11,3 +11,28 @@ export default () => (
         <Simple />
     </Driver>
 );
+
+
+/*
+
+1. need a redux model to track elements
+2. need a redux model to track drivers ??
+3. need a redux model to track active driver ?
+4. is it possible to split element & driver implementation into micro-services ?
+
+<App> 
+    <LinearDriver />
+    <GridDriver />
+    <Simple />
+    <Simple />
+    <Simple />
+    <Simple />
+    <Simple />
+    <Simple />
+</App>
+
+
+
+
+
+*/
